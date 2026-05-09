60a172cfe9f14e89bc11c2e4066f3141dcbc422e
BruisedSamurai <20334754+bruisedsamurai@users.noreply.github.com>
2026-05-09 15:29:40 +0530

    Update Button control to match Avalonia 12 API

--- src/Fabulous.Avalonia/Views/Controls/Buttons/Button.fs
+++ src/Fabulous.Avalonia/Views/Controls/Buttons/Button.fs
@@ -1,6 +1,7 @@
 namespace Fabulous.Avalonia
 
 open System.Runtime.CompilerServices
+open System.Windows.Input
 open Avalonia.Controls
 open Avalonia.Input
 open Fabulous
@@ -14,15 +15,21 @@ module Button =
     let ClickMode =
         Attributes.defineAvaloniaPropertyWithEquality Button.ClickModeProperty
 
+    let Command = Attributes.defineAvaloniaPropertyWithEquality Button.CommandProperty
+
+    let CommandParameter =
+        Attributes.defineAvaloniaPropertyWithEquality Button.CommandParameterProperty
+
+    let Flyout = Attributes.defineAvaloniaPropertyWidget Button.FlyoutProperty
 
     let HotKey = Attributes.defineAvaloniaPropertyWithEquality Button.HotKeyProperty
+
+    let IsCancel = Attributes.defineAvaloniaPropertyWithEquality Button.IsCancelProperty
 
     let IsDefault =
         Attributes.defineAvaloniaPropertyWithEquality Button.IsDefaultProperty
 
-    let IsCancel = Attributes.defineAvaloniaPropertyWithEquality Button.IsCancelProperty
-
-    let Flyout = Attributes.defineAvaloniaPropertyWidget Button.FlyoutProperty
+    let IsPressed = Attributes.defineAvaloniaPropertyWithEquality Button.IsPressedProperty
 
 type ButtonModifiers =
     /// <summary>Sets the ClickMode property.</summary>
@@ -32,19 +39,33 @@ type ButtonModifiers =
     static member inline clickMode(this: WidgetBuilder<'msg, #IFabButton>, value: ClickMode) =
         this.AddScalar(Button.ClickMode.WithValue(value))
 
+    /// <summary>Sets the Command property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The Command value.</param>
+    [<Extension>]
+    static member inline command(this: WidgetBuilder<'msg, #IFabButton>, value: ICommand) =
+        this.AddScalar(Button.Command.WithValue(value))
+
+    /// <summary>Sets the CommandParameter property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The CommandParameter value.</param>
+    [<Extension>]
+    static member inline commandParameter(this: WidgetBuilder<'msg, #IFabButton>, value: obj) =
+        this.AddScalar(Button.CommandParameter.WithValue(value))
+
+    /// <summary>Sets the Flyout property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The Flyout value.</param>
+    [<Extension>]
+    static member inline flyout(this: WidgetBuilder<'msg, #IFabButton>, value: WidgetBuilder<'msg, #IFabFlyoutBase>) =
+        this.AddWidget(Button.Flyout.WithValue(value.Compile()))
+
     /// <summary>Sets the HotKey property.</summary>
     /// <param name="this">Current widget.</param>
     /// <param name="value">The HotKey value.</param>
     [<Extension>]
     static member inline hotKey(this: WidgetBuilder<'msg, #IFabButton>, value: KeyGesture) =
         this.AddScalar(Button.HotKey.WithValue(value))
-
-    /// <summary>Sets the IsDefault property.</summary>
-    /// <param name="this">Current widget.</param>
-    /// <param name="value">The IsDefault value.</param>
-    [<Extension>]
-    static member inline isDefault(this: WidgetBuilder<'msg, #IFabButton>, value: bool) =
-        this.AddScalar(Button.IsDefault.WithValue(value))
 
     /// <summary>Sets the IsCancel property.</summary>
     /// <param name="this">Current widget.</param>
@@ -53,12 +74,19 @@ type ButtonModifiers =
     static member inline isCancel(this: WidgetBuilder<'msg, #IFabButton>, value: bool) =
         this.AddScalar(Button.IsCancel.WithValue(value))
 
-    /// <summary>Sets the Flyout property.</summary>
+    /// <summary>Sets the IsDefault property.</summary>
     /// <param name="this">Current widget.</param>
-    /// <param name="value">The Flyout value.</param>
+    /// <param name="value">The IsDefault value.</param>
     [<Extension>]
-    static member inline flyout(this: WidgetBuilder<'msg, #IFabButton>, value: WidgetBuilder<'msg, #IFabFlyoutBase>) =
-        this.AddWidget(Button.Flyout.WithValue(value.Compile()))
+    static member inline isDefault(this: WidgetBuilder<'msg, #IFabButton>, value: bool) =
+        this.AddScalar(Button.IsDefault.WithValue(value))
+
+    /// <summary>Sets the IsPressed property.</summary>
+    /// <param name="this">Current widget.</param>
+    /// <param name="value">The IsPressed value.</param>
+    [<Extension>]
+    static member inline isPressed(this: WidgetBuilder<'msg, #IFabButton>, value: bool) =
+        this.AddScalar(Button.IsPressed.WithValue(value))
 
     /// <summary>Link a ViewRef to access the direct Button control instance.</summary>
     /// <param name="this">Current widget.</param>
